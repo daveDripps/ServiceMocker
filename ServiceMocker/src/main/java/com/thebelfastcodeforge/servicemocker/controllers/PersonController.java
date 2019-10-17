fd84f05ee0b1316586db4fb9a1d6e678c27c8812
David Dripps <dave@thebelfastcodeforge.com>
2019-10-17 21:20:09 +0100

    basic hello and goodbye service

---
+++ ServiceMocker/src/main/java/com/thebelfastcodeforge/servicemocker/controllers/PersonController.java
@@ -0,0 +1,15 @@
+package com.thebelfastcodeforge.servicemocker.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PersonController {
+
+	
+
+	/*
+	 * @GetMapping("/getPerson")
+	 * 
+	 * @ResponseBody public String getPerson() { return "hello from getPerson"; }
+	 */
+} 
